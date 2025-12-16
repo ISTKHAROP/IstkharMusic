740a175460d13f1bed06d8dd4c94ef34506484c0
ISTKHAROP <istkharalam2006@gmail.com>
2025-12-16 21:25:54 +0530

    Update ping.py

--- IstkharMusic/plugins/tools/ping.py
+++ IstkharMusic/plugins/tools/ping.py
@@ -20,7 +20,7 @@ async def ping_com(client, message: Message, _):
         photo=random.choice(PING_IMG_URL),
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await ARISHFA.ping()
+    pytgping = await Istkhar.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
